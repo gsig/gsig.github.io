d2d3df14dd6e45923a6177b52eca19594b0d25ed
Gunnar Sigurdsson <gunnars@allenai.org>
2020-03-23 20:26:03 -0400

    Updating Website

--- generate.php
+++ generate.php
@@ -37,9 +37,9 @@
     <![endif]-->
 
 <?php
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
+# ini_set('display_startup_errors',1);
+# ini_set('display_errors',1);
+# error_reporting(-1);
 
 $_GET['library']=1;
 require_once('bibtexbrowser.php');
